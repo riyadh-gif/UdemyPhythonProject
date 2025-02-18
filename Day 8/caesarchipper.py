fb52c151547565cfbd53cf4d71fd685c77a2f7aa
riyadh <rlakadimu@gmail.com>
2025-02-18 23:36:04 +0700

    update

--- Day 8/caesarchipper.py
+++ Day 8/caesarchipper.py
@@ -1,30 +1,57 @@
+# TODO-1: Import and print the logo from art.py when the program starts.
+from art import logo
+
+print(logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+# TODO-2: What happens if the user enters a number/symbol/space?
 
-
-# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
-def encrypt(original_text, shift_ammount):
-    chippertext = ""
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+    if encode_or_decode == "decode":
+        shift_amount *= -1
 
     for letter in original_text:
-        shifted_posisition = alphabet.index(letter) + shift_ammount % 25
-        shift_over = shifted_posisition % len(alphabet)
+        if letter not in alphabet:
+            output_text += letter
 
-        chippertext += alphabet[shift_over]
-    
-    print(f"your text is: {chippertext}")
+        else :
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+
+# TODO-3: Can you figure out a way to restart the cipher program?
+def restart(chosen_user):
+    if chosen_user == "yes":
+        return True
+    elif chosen_user == "no":
+        print("Thank you for playing with me :)")
+        return False
+    else :
+        print("bye")
+        return False
+
+
+should_continue = True
+
+while should_continue:
+      direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+      text = input("Type your message:\n").lower()
+      shift = int(input("Type the shift number:\n"))
+
+      caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
+      chosen_user = input("do you want still play (yes/no):\n").lower()
+      should_continue = restart(chosen_user)
 
 
 
-# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
-#  by the shift amount and print the encrypted text.
 
-# TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
 
-# TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
-#  message.
 
-encrypt(original_text=text, shift_ammount=shift)
+
+
